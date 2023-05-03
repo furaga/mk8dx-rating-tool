9a768904c7c31fa08e2f8ec44c644eb75685fcc3
furaga <furaga.fukahori@gmail.com>
2023-05-04 08:16:10 +0900

    plot result

--- plot_result.py
+++ plot_result.py
@@ -23,51 +23,51 @@
 #     print("update")
 #     time.sleep(5)
 
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
+import random
 
 # データを作成
 x = [1, 2, 3, 4]
-y = [10, 15, 13, 17]
+
+def generate_y():
+    return [random.randint(10, 20) for _ in range(len(x))]
 
 # グラフを作成
-initial_trace = go.Scatter(x=x, y=y, mode="markers", marker=dict(color="red"))
+initial_trace = go.Scatter(x=x, y=generate_y(), mode='markers', marker=dict(color='red'))
 
 # Dashアプリケーションの作成
 app = dash.Dash(__name__)
 
 # アプリケーションのレイアウト
-app.layout = html.Div(
-    [
-        html.Button("Update Graph", id="update-button"),
-        dcc.Graph(id="my-graph", figure=go.Figure(data=[initial_trace])),
-    ]
-)
-
+app.layout = html.Div([
+    dcc.Graph(id="my-graph", figure=go.Figure(data=[initial_trace])),
+    dcc.Interval(
+        id='interval-component',
+        interval=5*1000, # in milliseconds
+        n_intervals=0
+    )
+])
 
 # コールバック関数
 @app.callback(
     Output("my-graph", "figure"),
-    Input("update-button", "n_clicks"),
+    Input("interval-component", "n_intervals"),
 )
-def update_graph(n_clicks):
-    if n_clicks is None:
-        raise dash.exceptions.PreventUpdate
-    
+def update_graph(n_intervals):
+    # update_tracesを使用してマーカーカラーを変更
     with open("out.csv", "r", encoding="sjis") as f:
         lines = f.readlines()
-        my_rates = [int(line.split(",")[4]) * 1.01 for line in lines[1:]]
+        my_rates = [int(line.split(",")[4]) for line in lines[1:]][:n]
         x =  [i for i in range(len(my_rates))]
 
     updated_trace = go.Scatter(x=x, y=my_rates, mode='lines+markers', name='Line 1')
 
     # グラフに反映させる
     return go.Figure(data=[updated_trace])
-
 
 # アプリケーションを実行
 if __name__ == "__main__":
